6103806aa67a3c65485da629ef2223adccce47b9
Damian Piętka <damianpietka@gmail.com>
2021-06-29 16:59:42 +0200

    Customized tab bars appearance

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 import MenuScreen from '../screens/MenuScreen';
 import CartScreen from '../screens/CartScreen';
@@ -9,9 +11,29 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === 'Menu') {
+            return (
+              <Ionicons name='fast-food-outline' size={size} color={color} />
+            );
+          } else if (route.name === 'Cart') {
+            return <AntDesign name='shoppingcart' size={size} color={color} />;
+          }
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
       <Tab.Screen name='Menu' component={MenuScreen}></Tab.Screen>
-      <Tab.Screen name='Cart' component={CartScreen}></Tab.Screen>
+      <Tab.Screen
+        name='Cart'
+        component={CartScreen}
+        options={{ tabBarBadge: 11 }}
+      ></Tab.Screen>
     </Tab.Navigator>
   );
 };
