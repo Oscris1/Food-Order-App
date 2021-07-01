0a8002516f0249587d40da2b3cad7e12d110447e
Damian Piętka <damianpietka@gmail.com>
2021-07-01 16:09:51 +0200

    created cart-slice and added it to store

--- store/index.js
+++ store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import cartSlice from './cart-slice';
 
 import menuSlice from './menu-slice';
 
 const store = configureStore({
-  reducer: { menu: menuSlice.reducer },
+  reducer: { menu: menuSlice.reducer, cart: cartSlice.reducer },
 });
 
 export default store;
