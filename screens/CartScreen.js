fc00d2b4e8599c6678fcc3fe7d3459b98ad2097d
Damian Piętka <damianpietka@gmail.com>
2021-07-01 17:05:11 +0200

    displayed cart data

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, FlatList } from 'react-native';
+import { useSelector } from 'react-redux';
+
+import CartItem from '../components/CartItem';
 
 const MenuScreen = () => {
+  const cartData = useSelector((state) => state.cart);
+  const items = cartData.items;
   return (
     <View style={styles.container}>
-      <Text>Cart Screen</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Koszyk</Text>
+      </View>
+
+      <View style={styles.totalBox}>
+        <Text style={styles.totalBoxText}>
+          Przedmioty w koszyku: {cartData.totalQuantity}
+        </Text>
+        <Text style={styles.totalBoxText}>
+          Do zapłaty: {cartData.totalPrice} zł
+        </Text>
+      </View>
+
+      <FlatList
+        style={styles.cartList}
+        data={items}
+        renderItem={({ item }) => <CartItem cartData={item} />}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
 };
@@ -14,8 +37,27 @@ export default MenuScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'hsl(0, 0%, 93%)',
   },
+  header: {
+    marginTop: 50,
+    marginBottom: 10,
+    marginHorizontal: 10,
+    borderBottomWidth: 2,
+    borderBottomColor: 'tomato',
+  },
+  headerText: {
+    fontSize: 25,
+    fontWeight: '700',
+    marginBottom: 2,
+  },
+  totalBox: {
+    marginHorizontal: 10,
+  },
+  totalBoxText: {
+    color: 'tomato',
+    fontWeight: '700',
+    fontSize: 15,
+  },
+  cartList: {},
 });
