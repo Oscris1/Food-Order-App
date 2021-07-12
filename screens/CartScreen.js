7b465c1e1a60a24e14f805d445547fcf60f0a26a
Damian Piętka <damianpietka@gmail.com>
2021-07-12 18:38:26 +0200

    created order button

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Text, View, StyleSheet, FlatList } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 import { useSelector } from 'react-redux';
 
 import CartItem from '../components/CartItem';
@@ -13,13 +19,18 @@ const MenuScreen = () => {
         <Text style={styles.headerText}>Koszyk</Text>
       </View>
 
-      <View style={styles.totalBox}>
-        <Text style={styles.totalBoxText}>
-          Przedmioty w koszyku: {cartData.totalQuantity}
-        </Text>
-        <Text style={styles.totalBoxText}>
-          Do zapłaty: {cartData.totalPrice} zł
-        </Text>
+      <View style={styles.orderInfoContainer}>
+        <View style={styles.totalBox}>
+          <Text style={styles.totalBoxText}>
+            Przedmioty w koszyku: {cartData.totalQuantity}
+          </Text>
+          <Text style={styles.totalBoxText}>
+            Do zapłaty: {cartData.totalPrice} zł
+          </Text>
+        </View>
+        <TouchableOpacity style={styles.orderButton}>
+          <Text style={styles.orderButtonText}>Zamów</Text>
+        </TouchableOpacity>
       </View>
 
       <FlatList
@@ -51,13 +62,28 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     marginBottom: 2,
   },
-  totalBox: {
+  orderInfoContainer: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
     marginHorizontal: 10,
+    paddingBottom: 10,
+    borderBottomWidth: 2,
+    borderBottomColor: 'tomato',
   },
+  totalBox: {},
   totalBoxText: {
-    color: 'tomato',
     fontWeight: '700',
+    color: 'black',
     fontSize: 15,
   },
   cartList: {},
+  orderButton: {
+    padding: 10,
+    backgroundColor: 'tomato',
+    borderRadius: 10,
+  },
+  orderButtonText: {
+    color: '#fff',
+  },
 });
